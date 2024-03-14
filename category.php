3ba2578bf390c1a6a654fd8e7c2f62aee34927fc
rano Loudjani <rano@MacBook-Pro-de-rano.local>
2024-03-14 15:21:35 +0100

    todo category

--- category.php
+++ category.php
@@ -2,18 +2,20 @@
 require_once __DIR__ . '/classes/Categories.php';
 require_once __DIR__ . '/layout/header.php';
 
-$id = intval($_GET['id']); // TODO: Vérifier que la clé 'id' existe
+if(isset($_GET['id']) && $_GET['id'] != "") {
+    $id = intval($_GET['id']); // TODO: Vérifier que la clé 'id' existe
 
-$categoriesDb = new Categories();
-$category = $categoriesDb->find($id);
+    $categoriesDb = new Categories();
+    $category = $categoriesDb->find($id);
+    
+    if ($category === null) {
+        http_response_code(404);
+        echo "Catégorie non trouvée";
+        exit;
+    }?>
 
-if ($category === null) {
-    http_response_code(404);
-    echo "Catégorie non trouvée";
-    exit;
-}
-?>
+    <h1><?php echo $category['name']; ?></h1>
 
-<h1><?php echo $category['name']; ?></h1>
+<?php }
 
-<?php require_once __DIR__ . '/layout/footer.php';
+require_once __DIR__ . '/layout/footer.php';
