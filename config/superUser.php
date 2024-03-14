2427d7c8ccd285edccfb5cc2927b464b807a91b5
rano Loudjani <rano@MacBook-Pro-de-rano.local>
2024-03-14 14:46:58 +0100

    authentification setup

--- config/superUser.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-require_once __DIR__ . '/../functions/utils.php';
-session_start();
-
-// Identifiants administrateur
-$_SESSION['superUser_login'] = "NOM UTILISATEUR";
-$_SESSION['superUser_password'] = "MOT DE PASSE";
--- login-process.php
+++ login-process.php
@@ -11,8 +11,10 @@ $login = trim($_POST['login']);
 $password = trim($_POST['password']);
 $accessDenied = true;
 
-echo $login, $password;
-
+[
+    'ADMIN_LOGIN' => $adminLogin,
+    'ADMIN_PASSWORD' => $adminPassword
+] = parse_ini_file(__DIR__ . '/config/admin.ini');
 
 $user = new Users();
 $alluser = $user->findAll();
@@ -25,7 +27,7 @@ foreach ($alluser as $index => $value) {
         $_SESSION['account_status'] = true;
         $_SESSION['message'] = "vous êtes connecté en tant que $login";
 
-        if ($value['login'] === $_SESSION['superUser_login'] && $value['password'] === $_SESSION['superUser_password']) {
+        if ($value['login'] === $adminLogin && $value['password'] === $adminPassword) {
             $_SESSION['superUser'] = true;
             $_SESSION['message'] = "Super User Mode activé";
         } else {
